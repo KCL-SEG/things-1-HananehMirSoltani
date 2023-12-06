7a79b83da622bde5a45cbe089e5ba69de2ef4fb7
HananehMirSoltani <hananeh.mir04@gmail.com>
2023-12-06 22:53:00 +0000

    add views.py

--- things/views.py
+++ things/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home_view(request):
-    html = "<html><head><title>Things</title></head><body><h1>Things</h1></body></html>"
-    return HttpResponse(html)
+def home(request):
+    return render(request, 'things/home.html')
